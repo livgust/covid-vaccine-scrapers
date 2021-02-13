13103f110c1213673dd0d30bec237a0fdc0a9c44
Zach Peyton <zachary.peyton@gmail.com>
2021-02-13 13:31:00 -0500

    Hannaford - adding handling found appointments

--- site-scrapers/Hannaford.js
+++ site-scrapers/Hannaford.js
@@ -23,18 +23,17 @@ module.exports = async function GetAvailableAppointments(browser) {
 
 async function ScrapeWebsiteData(browser) {
     const page = await browser.newPage();
-    await page.goto(sites.Hannaford.website);
-    await page.solveRecaptchas().then(({ solved }) => {
-        if (solved.length) {
-            return page.waitForNavigation();
-        } else {
-            return;
-        }
-    });
-
     const results = {};
-
     for (const loc of [...new Set(sites.Hannaford.locations)]) {
+        await page.goto(sites.Hannaford.website);
+        await page.solveRecaptchas().then(({ solved }) => {
+            if (solved.length) {
+                return page.waitForNavigation();
+            } else {
+                return;
+            }
+        });
+
         if (!results[loc.zip]) {
             await page.evaluate(
                 () => (document.getElementById("zip-input").value = "")
@@ -49,19 +48,28 @@ async function ScrapeWebsiteData(browser) {
                 ]),
                 page.click("#btnGo"),
             ]);
-            const result = (await searchResponse.buffer()).toString();
+
+            let result = '';
+            try {
+                result = (await searchResponse.buffer()).toString();
+            } catch (e) {
+                console.log("buffer failed, probably because the page changed during the 'waitForResponse'");
+            }
+
             //if there's something available, log it with a unique name so we can check it out l8r g8r
             if (result.indexOf(noAppointmentMatchString) == -1) {
-                let today = new Date();
-                today =
-                    today.getFullYear() +
-                    "-" +
-                    (today.getMonth() + 1) +
-                    "-" +
-                    today.getDate();
-                const filename =
-                    "hannaford-zip-" + loc.zip + "-date-" + today + ".png";
-                await page.screenshot({ path: filename });
+                // let today = new Date();
+                // today =
+                //     today.getFullYear() +
+                //     "-" +
+                //     (today.getMonth() + 1) +
+                //     "-" +
+                //     today.getDate();
+                // const filename =
+                //     "hannaford-zip-" + loc.zip + "-date-" + today + ".png";
+                // await page.screenshot({ path: filename });
+                await page.waitForSelector("#locationChangeBtn");
+                result = " appointments found ";
             }
             results[loc.zip] = result;
         }
