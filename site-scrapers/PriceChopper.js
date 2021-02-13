8b0a858720efb9d2f4c147069c01a9b7f828170f
Zach Peyton <zachary.peyton@gmail.com>
2021-02-12 23:58:41 -0500

    PriceChopper progress

---
+++ site-scrapers/PriceChopper.js
@@ -0,0 +1,54 @@
+const sites = require("../data/sites.json");
+const https = require("https");
+
+const noAppointmentMatchString = "no locations with available appointments";
+
+module.exports = async function GetAvailableAppointments(browser) {
+    console.log("PriceChopper starting.");
+    const webData = await ScrapeWebsiteData(browser);
+    console.log("PriceChopper done.");
+    return sites.PriceChopper.locations.map((loc) => {
+        const newLoc = { ...loc };
+        const response = webData[loc.id];
+        let hasAvailability = response[0].visibleTimeSlots.length ? 1 : 0;
+        return {
+            name: `PriceChopper (${loc.city})`,
+            hasAvailability,
+            signUpLink: sites.PriceChopper.website,
+            ...loc,
+        };
+    });
+};
+
+async function ScrapeWebsiteData(browser) {
+    
+    const rawData = {};
+
+    for (const loc of [...new Set(sites.PriceChopper.locations)]) {
+
+        let url = [
+            sites.PriceChopper.websiteRoot,
+            loc.eventId,
+            'GetLocations',
+            loc.zip
+        ].join('/') + '?state=' + loc.state;
+        
+        const p = new Promise((resolve) => {
+            let response = "";
+            https.get(url, (res) => {
+                let body = "";
+                res.on("data", (chunk) => {
+                    body += chunk;
+                });
+                res.on("end", () => {
+                    response = JSON.parse(body);
+                    resolve(response);
+                });
+            });
+        });
+
+        rawData[loc.id] = await p;
+    };
+
+    return rawData;
+}
--- site-scrapers/index.js
+++ site-scrapers/index.js
@@ -4,12 +4,14 @@ const UMassAmherst = require("./UMassAmherst.js");
 const Hannaford = require("./Hannaford.js");
 const Harrington = require("./Harrington.js");
 const Curative = require("./Curative.js");
+const PriceChopper = require("./PriceChopper.js");
 
 let scrapers = [
     //FamilyPracticeGroup,
     //MAImmunizations,
     //UMassAmherst,
-    Hannaford,
+    //Hannaford,
+    PriceChopper,
     //Harrington,
     //Curative,
 ];
