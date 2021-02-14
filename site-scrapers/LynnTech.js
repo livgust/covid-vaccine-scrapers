bc323bd24f17d4605d4357c7d342b35d3d652dbb
Erb <jay.erb@adp.com>
2021-02-14 13:35:18 -0500

    Added prettier config and formatted.

--- site-scrapers/LynnTech.js
+++ site-scrapers/LynnTech.js
@@ -1,47 +1,47 @@
-const sites = require('../data/sites.json');
-const https = require('https');
-const html_parser = require('node-html-parser');
+const sites = require("../data/sites.json");
+const https = require("https");
+const html_parser = require("node-html-parser");
 
 module.exports = async function GetAvailableAppointments() {
-    console.log('LynnTech starting.');
+    console.log("LynnTech starting.");
     const webData = await ScrapeWebsiteData();
-    console.log('LynnTech done.');
+    console.log("LynnTech done.");
     return {
         ...sites.LynnTech,
         ...webData,
     };
 };
 
-let siteId = '13300632';
-let vt = '1089';
-let dept = '133001025';
+let siteId = "13300632";
+let vt = "1089";
+let dept = "133001025";
 
 async function ScrapeWebsiteData() {
     // We need to go through the flow and use a request verification token
-    let cookie = '';
+    let cookie = "";
     const tokenPromise = new Promise(resolve => {
-        let response = '';
+        let response = "";
         https
             .get(
                 `https://mychartos.ochin.org/mychart/SignupAndSchedule/EmbeddedSchedule?id=${siteId}&vt=${vt}&dept=${dept}&view=plain&public=1&payor=-1,-2,-3,4655,4660,1292,4661,5369,5257,1624,4883&lang=english1089`,
                 res => {
-                    let body = '';
+                    let body = "";
                     // Collect the heeaders and cookies
                     let responseHeaders = res.headers;
-                    cookie = responseHeaders['set-cookie'];
-                    res.on('data', chunk => {
+                    cookie = responseHeaders["set-cookie"];
+                    res.on("data", chunk => {
                         body += chunk;
                     });
-                    res.on('end', () => {
+                    res.on("end", () => {
                         response = body;
 
                         resolve(response);
                     });
                 }
             )
-            .on('error', e => {
+            .on("error", e => {
                 console.error(
-                    'Error making mychart request for LynnTech: ' + e
+                    "Error making mychart request for LynnTech: " + e
                 );
             })
             .end();
@@ -51,9 +51,9 @@ async function ScrapeWebsiteData() {
     // Get the request verification token from the
     const root = html_parser.parse(tokenResponse);
     verificationInput = root.querySelector(
-        'input[name=__RequestVerificationToken]'
+        "input[name=__RequestVerificationToken]"
     );
-    verificationToken = verificationInput.getAttribute('value');
+    verificationToken = verificationInput.getAttribute("value");
 
     // Setup the return object.
     const results = { availability: {}, hasAvailability: false };
@@ -65,21 +65,21 @@ async function ScrapeWebsiteData() {
     for (const i of Array(10).keys()) {
         // Format the date as 'YYYY-MM-dd'
         let startDateFormatted = `${startDate.getFullYear()}-${(
-            '0' +
+            "0" +
             (startDate.getMonth() + 1)
-        ).slice(-2)}-${('0' + startDate.getDate()).slice(-2)}`;
+        ).slice(-2)}-${("0" + startDate.getDate()).slice(-2)}`;
 
         const postData = `id=${siteId}&vt=${vt}&dept=${dept}&view=plain&start=${startDateFormatted}&filters=%7B%22Providers%22%3A%7B%2213300632%22%3Atrue%7D%2C%22Departments%22%3A%7B%22133001025%22%3Atrue%7D%2C%22DaysOfWeek%22%3A%7B%220%22%3Atrue%2C%221%22%3Atrue%2C%222%22%3Atrue%2C%223%22%3Atrue%2C%224%22%3Atrue%2C%225%22%3Atrue%2C%226%22%3Atrue%7D%2C%22TimesOfDay%22%3A%22both%22%7D`;
         const options = {
-            hostname: 'mychartos.ochin.org',
+            hostname: "mychartos.ochin.org",
             port: 443,
             path:
-                '/mychart/OpenScheduling/OpenScheduling/GetOpeningsForProvider',
-            method: 'POST',
+                "/mychart/OpenScheduling/OpenScheduling/GetOpeningsForProvider",
+            method: "POST",
             headers: {
-                'Content-Type':
-                    'application/x-www-form-urlencoded; charset=UTF-8',
-                'Content-Length': postData.length,
+                "Content-Type":
+                    "application/x-www-form-urlencoded; charset=UTF-8",
+                "Content-Length": postData.length,
                 __RequestVerificationToken: verificationToken,
                 Cookie: cookie,
             },
@@ -88,25 +88,25 @@ async function ScrapeWebsiteData() {
         // now request the schedule
         const schedulePromise = new Promise(resolve => {
             const req = https.request(options, res => {
-                let body = '';
+                let body = "";
                 responseHeaders = res.headers;
-                res.on('data', chunk => {
+                res.on("data", chunk => {
                     body += chunk;
                 });
-                res.on('end', () => {
+                res.on("end", () => {
                     if (res.statusCode >= 200 && res.statusCode < 300) {
                         resolve(JSON.parse(body));
                     } else {
                         console.error(
-                            'Error status code returned from ' + res.url
+                            "Error status code returned from " + res.url
                         );
                         resolve();
                     }
                 });
             });
             req.write(postData);
-            req.on('error', e => {
-                console.error('Error making scheduling request : ' + e);
+            req.on("error", e => {
+                console.error("Error making scheduling request : " + e);
             });
             req.end();
         });
@@ -145,7 +145,7 @@ async function ScrapeWebsiteData() {
                 }
             }
         } else {
-            console.error('Null response returned from scheduling request');
+            console.error("Null response returned from scheduling request");
         }
         // Increment the date another week.
         startDate.setDate(startDate.getDate() + 7);
