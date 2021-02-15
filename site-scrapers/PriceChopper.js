cf91303ab6a65a6f7d9a983f9bbd0060c909f281
Olivia Adams <liv.gust@gmail.com>
2021-02-15 11:45:40 -0500

    fixed formatting

--- site-scrapers/PriceChopper.js
+++ site-scrapers/PriceChopper.js
@@ -1,8 +1,8 @@
 const sites = require("../data/sites.json");
 const https = require("https");
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-const siteName = 'Price Chopper';
+const siteName = "Price Chopper";
 const site = sites[siteName];
 
 module.exports = async function GetAvailableAppointments(browser) {
@@ -13,18 +13,24 @@ module.exports = async function GetAvailableAppointments(browser) {
         const locHash = md5HashString(loc.street + loc.city);
         const responseLocation = webData[locHash];
         let hasAvailability = false;
-        let availability = {};
-        if(responseLocation && responseLocation.visibleTimeSlots) {
-            hasAvailability = responseLocation.visibleTimeSlots.length ? true : false;
-            for(let [i, timeSlot] of responseLocation.visibleTimeSlots.entries()) {
-                let date = timeSlot.time.split('T')[0];
-                if(!availability[date]){
-                    availability[date] = {
+        const availability = {};
+        if (responseLocation && responseLocation.visibleTimeSlots) {
+            hasAvailability = !!responseLocation.visibleTimeSlots.length;
+            for (const [
+                i,
+                timeSlot,
+            ] of responseLocation.visibleTimeSlots.entries()) {
+                const date = new Date(timeSlot.time.split("T")[0]);
+                const formattedDate = `${date.getMonth() + 1}/${
+                    date.getDate() + 1
+                }/${date.getFullYear()}`;
+                if (!availability[formattedDate]) {
+                    availability[formattedDate] = {
                         hasAvailability: true,
-                        numberAvailableAppointments: 0
+                        numberAvailableAppointments: 0,
                     };
                 }
-                availability[date].numberAvailableAppointments += 1;
+                availability[formattedDate].numberAvailableAppointments += 1;
             }
         }
         return {
@@ -37,38 +43,38 @@ module.exports = async function GetAvailableAppointments(browser) {
     });
 };
 
-function md5HashString(string){
-    return crypto.createHash('md5').update(string).digest('hex');
+function md5HashString(string) {
+    return crypto.createHash("md5").update(string).digest("hex");
 }
 
 async function ScrapeWebsiteData(browser) {
     const rawData = {};
-    for (let [i , loc] of site.locations.entries()) {
-        let url = [
-            site.websiteRoot,
-            loc.zip
-        ].join('/') + '?state=' + loc.state;
+    for (const [i, loc] of site.locations.entries()) {
+        const url =
+            [site.websiteRoot, loc.zip].join("/") + "?state=" + loc.state;
         const locHash = md5HashString(loc.street + loc.city);
-        if(!rawData[locHash]){
+        if (!rawData[locHash]) {
             const getUrl = new Promise((resolve) => {
                 let response = "";
                 https.get(url, (res) => {
                     let body = "";
-                    res.on("data", (chunk) => body += chunk);
+                    res.on("data", (chunk) => (body += chunk));
                     res.on("end", () => {
                         response = JSON.parse(body);
                         resolve(response);
                     });
                 });
             });
-            let responseJson = await getUrl;
+            const responseJson = await getUrl;
             responseJson.map((responseLoc) => {
-                const responseLocHash = md5HashString(responseLoc.address1 + responseLoc.city);
-                if(responseLocHash == locHash){
+                const responseLocHash = md5HashString(
+                    responseLoc.address1 + responseLoc.city
+                );
+                if (responseLocHash === locHash) {
                     rawData[locHash] = responseLoc;
                 }
             });
         }
-    };
+    }
     return rawData;
 }
