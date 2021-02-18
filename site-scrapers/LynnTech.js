06ef7ed291067bbe633e4a530ea6f2369e7b9270
Zach Peyton <zachary.peyton@gmail.com>
2021-02-18 09:32:45 -0500

    Dynamic scrapers (#44)
    * allow for dynamic scraper files and argv override
    
    * Moved MyChartAPI to the lib directory.
    
    Co-authored-by: Erb <jay.erb@adp.com>

--- site-scrapers/LynnTech.js
+++ site-scrapers/LynnTech.js
@@ -1,5 +1,5 @@
 const sites = require("../data/sites.json");
-const mychart = require("./MyChartAPI");
+const mychart = require("../lib/MyChartAPI");
 
 module.exports = async function GetAvailableAppointments() {
     console.log("LynnTech starting.");
