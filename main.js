ceb888cfd008d76714cb9501d578e747392652bd
Zach Peyton <zachary.peyton@gmail.com>
2021-02-23 14:16:54 -0500

    Hannaford - store site content on s3 (#76)
    * Main now uses lib files for s3
    
    * Add lib File util
    
    * Hannaford: fix zipcode
    
    * Hannaford: upload site content and screen to s3
    
    * Add lib s3 util
    
    * Hannaford: add zip for Dracut

--- main.js
+++ main.js
@@ -9,17 +9,10 @@ const Recaptcha = require("puppeteer-extra-plugin-recaptcha");
 const scrapers = require("./site-scrapers");
 const fetch = require("node-fetch");
 const dataDefaulter = require("./data/dataDefaulter");
-
-//aws-sdk is only a dev dependency because Lambda already includes the package by default.
-const AWS = require("aws-sdk");
+const file = require("./lib/file");
+const s3 = require("./lib/s3");
 
 async function execute() {
-    //S3 bucket initialization
-    const s3 = new AWS.S3({
-        accessKeyId: process.env.AWSACCESSKEYID,
-        secretAccessKey: process.env.AWSSECRETACCESSKEY,
-    });
-
     const cachedResults = await fetch(
         "https://mzqsa4noec.execute-api.us-east-1.amazonaws.com/prod"
     )
@@ -83,61 +76,16 @@ async function execute() {
             results: finalResultsArray,
         };
 
+        const webData = JSON.stringify(responseJson);
+
         if (process.env.DEVELOPMENT) {
             console.log("The following data would be published:");
             console.dir(responseJson, { depth: null });
-            fs = require("fs");
-            await new Promise((resolve, reject) => {
-                fs.writeFile(
-                    "out.json",
-                    JSON.stringify(responseJson),
-                    (err) => {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            resolve();
-                        }
-                    }
-                );
-            });
-            return;
+            file.write("out.json", webData);
+            return responseJson;
         } else {
-            const params = {
-                Bucket: process.env.AWSS3BUCKETNAME,
-                Key: "data.json",
-                Body: JSON.stringify(responseJson),
-            };
-
-            // Uploading files to the bucket
-            const results = await new Promise((resolve, reject) => {
-                s3.upload(params, function (err, data) {
-                    if (err) {
-                        reject(err);
-                    }
-                    console.log(`File uploaded successfully. ${data.Location}`);
-                    resolve(data);
-                });
-            });
-            // Timestamped upload
-            var params2 = params;
-            const now = new Date();
-            const timestamp =
-                now.toISOString().substring(0, 16).replace(":", "") + "Z";
-            // timestamp form: "2021-02-12T1838Z" (we omit seconds for predictability)
-            params2.Key = "data-" + timestamp + ".json";
-            // Do the reupload
-            const results2 = await new Promise((resolve, reject) => {
-                s3.upload(params2, function (err, data) {
-                    if (err) {
-                        reject(err);
-                    }
-                    console.log(
-                        `Timestamped file uploaded successfully. ${data.Location}`
-                    );
-                    resolve(data);
-                });
-            });
-            return results;
+            const uploadResponse = await s3.saveWebData(webData);
+            return uploadResponse;
         }
     };
     await gatherData();
