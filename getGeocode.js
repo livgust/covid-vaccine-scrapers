5f5b649c6248dce13d3a3d449872dfe067a8c957
Derek Lee <dldereklee@gmail.com>
2021-02-22 21:32:35 -0500

    Add geocoding to main script

--- getGeocode.js
+++ getGeocode.js
@@ -29,30 +29,52 @@ const getLocationData = async () => {
     "https://mzqsa4noec.execute-api.us-east-1.amazonaws.com/prod"
   );
   const data = await res.json();
-  return JSON.parse(data.body).results;
-};
 
-const getAllCoordinates = async () => {
-  const locations = await getLocationData();
   let locationLookup = {};
+  for (const location of JSON.parse(data.body).results) {
+    const {
+      name = "",
+      street = "",
+      zip = "",
+      resolvedLocation,
+      latitude,
+      longitude,
+    } = location;
 
-  for (const location of locations) {
-    const { name = "", street = "", zip = "" } = location;
-    const locationInd = `${name}${street}${zip}`;
-
-    if (geo[locationInd]) {
-      locationLookup[locationInd] = geo[locationInd];
-    } else {
-      const locationData = await getGeocode(name, street, zip);
+    const locationInd = `${name}|${street}|${zip}`;
+    if (resolvedLocation && latitude && longitude) {
       locationLookup[locationInd] = {
-        resolvedLocation: locationData.results[0].formatted_address,
-        latitude: locationData.results[0].geometry.location.lat,
-        longitude: locationData.results[0].geometry.location.lng,
+        resolvedLocation,
+        latitude,
+        longitude,
       };
     }
-
-    fs.writeFileSync("geo.json", JSON.stringify(locationLookup));
   }
+  return locationLookup;
 };
 
-getAllCoordinates();
+const getAllCoordinates = async (locations) => {
+  const existingLocations = await getLocationData();
+
+  const coordinateData = await Promise.all(
+    locations.map(async (location) => {
+      const { name = "", street = "", zip = "" } = location;
+      const locationInd = `${name}|${street}|${zip}`;
+
+      if (existingLocations[locationInd]) {
+        return { ...location, ...existingLocations[locationInd] };
+      } else {
+        const locationData = await getGeocode(name, street, zip);
+        return {
+          ...location,
+          resolvedLocation: locationData.results[0].formatted_address,
+          latitude: locationData.results[0].geometry.location.lat,
+          longitude: locationData.results[0].geometry.location.lng,
+        };
+      }
+    })
+  );
+  return coordinateData;
+};
+
+exports.getAllCoordinates = getAllCoordinates;
--- main.js
+++ main.js
@@ -7,120 +7,124 @@ const { addExtra } = require("puppeteer-extra");
 const Puppeteer = addExtra(chromium.puppeteer);
 const Recaptcha = require("puppeteer-extra-plugin-recaptcha");
 const scrapers = require("./site-scrapers");
+const { getAllCoordinates } = require("./getGeocode");
 
 //aws-sdk is only a dev dependency because Lambda already includes the package by default.
 const AWS = require("aws-sdk");
 
 async function execute() {
-	//S3 bucket initialization
-	const s3 = new AWS.S3({
-		accessKeyId: process.env.AWSACCESSKEYID,
-		secretAccessKey: process.env.AWSSECRETACCESSKEY,
-	});
+  //S3 bucket initialization
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWSACCESSKEYID,
+    secretAccessKey: process.env.AWSSECRETACCESSKEY,
+  });
 
-	Puppeteer.use(
-		Recaptcha({
-			provider: { id: "2captcha", token: process.env.RECAPTCHATOKEN },
-		})
-	);
+  Puppeteer.use(
+    Recaptcha({
+      provider: { id: "2captcha", token: process.env.RECAPTCHATOKEN },
+    })
+  );
 
-	const browser = process.env.DEVELOPMENT
-		? await Puppeteer.launch({
-				executablePath: process.env.CHROMEPATH,
-		  })
-		: await Puppeteer.launch({
-				args: chromium.args,
-				defaultViewport: chromium.defaultViewport,
-				executablePath: await chromium.executablePath,
-				headless: chromium.headless,
-				ignoreHTTPSErrors: true,
-		  });
+  const browser = process.env.DEVELOPMENT
+    ? await Puppeteer.launch({
+        executablePath: process.env.CHROMEPATH,
+      })
+    : await Puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+      });
 
-	const gatherData = async () => {
-		const results = await Promise.all(
-			scrapers.map((scraper) =>
-				scraper(browser).catch((error) => {
-					//print out the issue but don't fail, this way we still publish updates
-					//for other locations even if this website's scrape doesn't work
-					console.log(error);
-					return null;
-				})
-			)
-		);
-		browser.close();
-		const finalResultsArray = [];
-		for (const result of results) {
-			if (Array.isArray(result)) {
-				finalResultsArray.push(...result);
-			} else if (result) {
-				//ignore nulls
-				finalResultsArray.push(result);
-			}
-		}
+  const gatherData = async () => {
+    const results = await Promise.all(
+      scrapers.map((scraper) =>
+        scraper(browser).catch((error) => {
+          //print out the issue but don't fail, this way we still publish updates
+          //for other locations even if this website's scrape doesn't work
+          console.log(error);
+          return null;
+        })
+      )
+    );
+    browser.close();
+    const finalResultsArray = [];
+    for (const result of results) {
+      if (Array.isArray(result)) {
+        finalResultsArray.push(...result);
+      } else if (result) {
+        //ignore nulls
+        finalResultsArray.push(result);
+      }
+    }
 
-		const responseJson = {
-			results: finalResultsArray,
-		};
+    const responseJson = {
+      results: await getAllCoordinates(finalResultsArray),
+    };
 
-		if (process.env.DEVELOPMENT) {
-			console.log("The following data would be published:");
-			console.dir(responseJson, { depth: null });
-			fs = require("fs");
-			await new Promise((resolve, reject) => {
-				fs.writeFile("out.json", JSON.stringify(responseJson), (err) => {
-					if (err) {
-						reject(err);
-					} else {
-						resolve();
-					}
-				});
-			});
-			return;
-		} else {
-			const params = {
-				Bucket: process.env.AWSS3BUCKETNAME,
-				Key: "data.json",
-				Body: JSON.stringify(responseJson),
-			};
+    if (process.env.DEVELOPMENT) {
+      console.log("The following data would be published:");
+      console.dir(responseJson, { depth: null });
+      fs = require("fs");
+      await new Promise((resolve, reject) => {
+        fs.writeFile("out.json", JSON.stringify(responseJson), (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+      return;
+    } else {
+      const params = {
+        Bucket: process.env.AWSS3BUCKETNAME,
+        Key: "data.json",
+        Body: JSON.stringify(responseJson),
+      };
 
-			// Uploading files to the bucket
-			const results = await new Promise((resolve, reject) => {
-				s3.upload(params, function (err, data) {
-					if (err) {
-						reject(err);
-					}
-					console.log(`File uploaded successfully. ${data.Location}`);
-					resolve(data);
-				});
-			});
-		        // Timestamped upload
-		  	var params2 = params;
-		  	const now = new Date();
-		        const timestamp = now.toISOString().substring(0,16).replace(':','')+'Z';
-		  	// timestamp form: "2021-02-12T1838Z" (we omit seconds for predictability)
-		  	params2.Key = "data-"+timestamp+".json";
-		  	// Do the reupload
-		  	const results2 = await new Promise((resolve, reject) => {
-		  			s3.upload(params2, function (err, data) {
-		  				if (err) {
-		  					reject(err);
-		  				}
-		  				console.log(`Timestamped file uploaded successfully. ${data.Location}`);
-		  				resolve(data);
-		  			});
-		  		});
-		  	return results;
-		}
-	};
-	await gatherData();
+      // Uploading files to the bucket
+      const results = await new Promise((resolve, reject) => {
+        s3.upload(params, function (err, data) {
+          if (err) {
+            reject(err);
+          }
+          console.log(`File uploaded successfully. ${data.Location}`);
+          resolve(data);
+        });
+      });
+      // Timestamped upload
+      var params2 = params;
+      const now = new Date();
+      const timestamp =
+        now.toISOString().substring(0, 16).replace(":", "") + "Z";
+      // timestamp form: "2021-02-12T1838Z" (we omit seconds for predictability)
+      params2.Key = "data-" + timestamp + ".json";
+      // Do the reupload
+      const results2 = await new Promise((resolve, reject) => {
+        s3.upload(params2, function (err, data) {
+          if (err) {
+            reject(err);
+          }
+          console.log(
+            `Timestamped file uploaded successfully. ${data.Location}`
+          );
+          resolve(data);
+        });
+      });
+      return results;
+    }
+  };
+  await gatherData();
 }
 
 exports.handler = execute;
 
 if (process.env.DEVELOPMENT) {
-	(async () => {
-		console.log("DEV MODE");
-		await execute();
-		process.exit();
-	})();
+  (async () => {
+    console.log("DEV MODE");
+    await execute();
+    process.exit();
+  })();
 }
