f489c56ccc533ae70e91f0a2b03fcac3173c42c0
tkjones117 <tkjones117@gmail.com>
2021-02-24 13:58:26 -0500

    chore: still return fields if response is null

--- site-scrapers/StopAndShop.js
+++ site-scrapers/StopAndShop.js
@@ -12,8 +12,8 @@ module.exports = async function GetAvailableAppointments(browser) {
         const response = webData[loc.zip];
         return {
             name: `Stop & Shop (${loc.city})`,
-            hasAvailability: response.indexOf(rxTouch.noAppointmentMatchString) == -1,
-            extraData: response.length
+            hasAvailability: response ? response.indexOf(rxTouch.noAppointmentMatchString) == -1 : false,
+            extraData: response && response.length
                 ? response.substring(1, response.length - 1)
                 : response, //take out extra quotes
             signUpLink: site.website,
