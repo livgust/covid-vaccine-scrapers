3ffd039c792beb520136a55e1f835fb5ff6479b1
Rob Harris <rob@harcod.com>
2021-03-04 07:19:54 -0500

    prettier!

--- site-scrapers/Curative/index.js
+++ site-scrapers/Curative/index.js
@@ -44,7 +44,7 @@ module.exports = async function GetAvailableAppointments(browser) {
         data.appointment_windows.forEach((appointment) => {
             const dateRegexp = /(?<year>[0-9]{4})-(?<month>[0-9]{2})-(?<day>[0-9]{2})/;
             const { year, month, day } = appointment.start_time.match(
-                dateRegexp,
+                dateRegexp
             ).groups;
             const date = `${month}/${day}/${year}`;
             let newNumberAvailable =
@@ -74,7 +74,6 @@ module.exports = async function GetAvailableAppointments(browser) {
             // Commented out the following line because there is currently a
             // waiting room for people to join for an event that starts at 8:30am.
             // We should revisit this later after the appointments are gone.
-
             //mappedData.hasAvailability = false;
         }
 
