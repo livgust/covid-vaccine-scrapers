9a512ee5f7bba97d6c003a06d6862918cd5180c1
Armen Kaleshian <armen@kriation.com>
2021-03-11 23:10:44 -0500

    Add site config payload

---
+++ site-scrapers/BaystateHealth/config.js
@@ -0,0 +1,12 @@
+const site = {
+    name: "Baystate Health",
+    street: "361 Whitney Ave",
+    city: "Holyoke",
+    state: "MA",
+    zip: "01040",
+    signUpLink: "https://www.baystatehealth.org/covid19",
+};
+
+module.exports = {
+    site,
+};
