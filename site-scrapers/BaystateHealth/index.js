f2f74147c70de0a80920e9d968b6a952192a9daf
Armen Kaleshian <armen@kriation.com>
2021-03-11 23:12:31 -0500

    Add robustness to scraper
    Based on code written for another scraper and feedback from the team,
    this commit adds the following:
    * Required keys to returned payload
    * Ability to be tested using Puppeteer

--- site-scrapers/BaystateHealth/index.js
+++ site-scrapers/BaystateHealth/index.js
@@ -1,43 +1,69 @@
-const sites = require("../data/sites.json");
+const { site } = require("./config");
 
-const noAppointmentMatchString =
-    "Registration Temporarily Unavailable";
+const noAppointmentMatchString = "Registration Temporarily Unavailable";
 
+/*
+ * This function calls ScrapeWebsiteData to gather availability data from the
+ * site
+ *
+ * @param browser created when Puppeteer connects to a Chromium instance
+ * @return JSON blob with { hasAppointments: Bool, totalAvailability: Int }
+ */
 module.exports = async function GetAvailableAppointments(browser) {
-    console.log(sites.BaystateHealth.name + " starting.");
-    const webData = await ScrapeWebsiteData(browser);
-    console.log(sites.BaystateHealth.name + " finished.");
-    return {
-        ...sites.BaystateHealth,
-        ...webData,
-        timestamp: new Date(),
-    };
+    if (browser.browserContexts().length > 1) {
+        await console.log(`${site.name} testsuite started...`);
+        const webData = await ScrapeWebsiteData(browser);
+        await console.log(`${site.name} testsuite finished...`);
+        return {
+            ...site,
+            ...webData,
+            timestamp: new Date(),
+        };
+    } else {
+        await console.log(`${site.name} starting.`);
+        const page = await browser.newPage();
+        await page.goto(site.signUpLink);
+        const webData = await ScrapeWebsiteData(browser);
+        await console.log(`${site.name} done.`);
+        return {
+            ...site,
+            ...webData,
+            timestamp: new Date(),
+        };
+    }
 };
 
 async function ScrapeWebsiteData(browser) {
-    const page = await browser.newPage();
+    let pageArray = await browser.pages();
+    const page = pageArray[1];
     const signUpPageSelector = "div > div > section > div > div > p > a";
-    await page.goto(sites.BaystateHealth.website);
-    await page.waitForSelector(signUpPageSelector,
-        { visible: true });
+    await page.waitForSelector(signUpPageSelector, { visible: true });
 
-    // Replace _blank with _self to ease interaction through Puppeteer
-    // Thanks StackOverflow!
     let signUpPageElement = await page.$(signUpPageSelector);
     await page.evaluateHandle((el) => {
-        el.target = '_self';
-    }, signUpPageElement)
+        el.target = "_self";
+    }, signUpPageElement);
     await Promise.all([
         page.waitForNavigation({ waitUntil: "networkidle0" }),
         page.click(signUpPageSelector),
     ]);
 
     // Is the clinic open?
-    const alert = await (
-        await page.$("app-guest-covid-vaccine-register > div > div > div > h3")
-    ).evaluate((node) => node.innerText);
+    const alertElement = await page.$(
+        "app-guest-covid-vaccine-register > div > div > div > h3"
+    );
+    let alert = alertElement
+        ? await alertElement.evaluate((node) => node.innerText)
+        : false;
+    let hasAppointments = false;
+    let totalAvailability = 0;
+    let date = false;
+    if (!alert) {
+        // TBD: Are there appointments?
+    }
 
     return {
-        hasAvailability: alert.indexOf(noAppointmentMatchString) === -1,
+        hasAppointments: hasAppointments,
+        totalAvailability: totalAvailability,
     };
 }
