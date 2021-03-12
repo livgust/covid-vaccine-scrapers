6d4ded5510f3d2f552354168eb87859d12c89bb5
Armen Kaleshian <armen@kriation.com>
2021-03-11 23:31:01 -0500

    Add test suite for scraper

---
+++ test/BaystateHealth.js
@@ -0,0 +1,34 @@
+const { expect } = require("chai");
+const chai = require("chai");
+chai.use(require("chai-as-promised"));
+const baystatehealth = require("./../site-scrapers/BaystateHealth");
+const { site } = require("./../site-scrapers/BaystateHealth/config");
+
+describe(`${site.name}`, function () {
+    let context;
+    let page;
+
+    beforeEach(async function() {
+        context = await browser.createIncognitoBrowserContext();
+        page = await browser.newPage();
+    });
+
+    afterEach(async function() {
+        await page.close();
+    });
+
+    it("Test against live site", async function() {
+        await page.goto(site.signUpLink);
+        await expect(baystatehealth(browser)).to.eventually.have.keys([
+            "name",
+            "street",
+            "city",
+            "state",
+            "zip",
+            "signUpLink",
+            "hasAppointments",
+            "totalAvailability",
+            "timestamp",
+        ]);
+    });
+});
