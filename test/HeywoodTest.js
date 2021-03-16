50f1933aa5b30ed069d3d91880585d244ae2ec6d
Armen Kaleshian <kriation@users.noreply.github.com>
2021-03-16 10:43:23 -0400

    Fixed issue with Heywood scraper and corresponding test (#147)
    * Fix issue with scaper not evaluating correct page
    
    Discovered a core issue with this scraper based on test code added
    previously. Removed the test suite logic so that the correct page is
    always evaluated.
    
    * Cut malfunctioning test suite code from scraper

--- test/HeywoodTest.js
+++ test/HeywoodTest.js
@@ -5,20 +5,7 @@ const heywood = require("./../site-scrapers/HeywoodHealthcare");
 const { site } = require("./../site-scrapers/HeywoodHealthcare/config");
 
 describe(`${site.name}`, function () {
-    let context;
-    let page;
-
-    beforeEach(async function () {
-        context = await browser.createIncognitoBrowserContext();
-        page = await browser.newPage();
-    });
-
-    afterEach(async function () {
-        await page.close();
-    });
-
     it("Test against live site", async function () {
-        await page.goto(site.signUpLink);
         await expect(heywood(browser)).to.eventually.have.keys([
             "name",
             "street",
@@ -30,25 +17,5 @@ describe(`${site.name}`, function () {
             "totalAvailability",
             "timestamp",
         ]);
-    });
-
-    it("Test against mock'd site with no alert", async function () {
-        await page.goto(
-            `file:///${process.cwd()}/test/Heywood/no-available-appointments.html`
-        );
-        await expect(heywood(browser)).to.eventually.include({
-            hasAppointments: false,
-            totalAvailability: 0,
-        });
-    });
-
-    it("Test against mock'd site with 8 available appointments", async function () {
-        await page.goto(
-            `file:///${process.cwd()}/test/Heywood/available-appointments.html`
-        );
-        await expect(heywood(browser)).to.eventually.include({
-            hasAppointments: true,
-            totalAvailability: 8,
-        });
     });
 });
