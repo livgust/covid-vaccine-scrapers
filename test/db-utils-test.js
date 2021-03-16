313a3af82a38492b889f1ea118e755ddaf65ba40
tkjones117 <tkjones117@gmail.com>
2021-03-16 10:11:10 -0400

    added replace util, did some refactoring

--- test/db-utils-test.js
+++ test/db-utils-test.js
@@ -4,9 +4,10 @@ chai.use(require("chai-shallow-deep-equal"));
 const expect = chai.expect;
 
 describe("FaunaDB Utils", function () {
-    it("can create, retrieve, and delete locations", async () => {
+    it("can create, retrieve, replace, and delete docs from LOCATIONS collection", async () => {
         const dbUtils = require("../lib/db-utils");
 
+        const collectionName = "locations";
         const data = {
             name: "FakeLocationName",
             address: { street: "1 Main St", city: "Newton", zip: "02458" },
@@ -15,7 +16,7 @@ describe("FaunaDB Utils", function () {
         const generatedId = await dbUtils.generateId();
 
         await expect(
-            dbUtils.retrieveLocationByRefId(generatedId)
+            dbUtils.retrieveItemByRefId(collectionName, generatedId)
         ).to.eventually.be.rejectedWith("instance not found");
 
         await dbUtils.writeLocationByRefId({
@@ -23,7 +24,8 @@ describe("FaunaDB Utils", function () {
             ...data,
         });
 
-        const retrieveResult = await dbUtils.retrieveLocationByRefId(
+        const retrieveResult = await dbUtils.retrieveItemByRefId(
+            collectionName,
             generatedId
         );
         expect(retrieveResult).to.be.shallowDeepEqual({
@@ -45,9 +47,46 @@ describe("FaunaDB Utils", function () {
             data,
         });
 
-        await dbUtils.deleteLocationByRefId(generatedId);
+        // modify the data
+        const modifiedData = {
+            name: "NewFakeLocationName",
+            address: { street: "1 Main St", city: "Newton", zip: "02458" },
+            signupLink: "www.google.com",
+        };
+
+        await dbUtils.replaceLocationByRefId({
+            id: generatedId,
+            ...modifiedData,
+        });
+
+        const retrieveResult2 = await dbUtils.retrieveItemByRefId(
+            collectionName,
+            generatedId
+        );
+        expect(retrieveResult2).to.be.shallowDeepEqual({
+            ref: {
+                value: {
+                    collection: {
+                        value: {
+                            collection: {
+                                value: {
+                                    id: "collections",
+                                },
+                            },
+                            id: "locations",
+                        },
+                    },
+                    id: generatedId,
+                },
+            },
+            data: modifiedData,
+        });
+
+        await dbUtils.deleteItemByRefId(collectionName, generatedId);
         await expect(
-            dbUtils.retrieveLocationByRefId(generatedId)
+            dbUtils.retrieveItemByRefId(collectionName, generatedId)
         ).to.eventually.be.rejectedWith("instance not found");
     });
+
+
 });
