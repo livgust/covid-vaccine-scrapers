3db2df04f193b9023765119456733f9a876a6d4c
Olivia Adams <liv.gust@gmail.com>
2021-03-16 08:11:56 -0400

    don't show massvax availability (invite-only)

--- site-scrapers/Color/index.js
+++ site-scrapers/Color/index.js
@@ -47,42 +47,42 @@ async function ScrapeWebsiteData(siteName, siteUrl) {
 function formatResponse(availabilityResponse) {
     const availability = JSON.parse(availabilityResponse).results;
     const results = { availability: {}, hasAvailability: false };
+    /* COMMENTING OUT BECAUSE IT'S NOW INVITE-ONLY */
     // Collect availability count by date
-    availability.reduce((memo, currentValue) => {
-        /* The availability returns an array of appointments like this:
-            {
-                "start": "2021-02-22T14:00:00+00:00",
-                "end": "2021-02-22T14:04:00+00:00",
-                "capacity": 1,
-                "remaining_spaces": -1
+    //  availability.reduce((memo, currentValue) => {
+    //     /* The availability returns an array of appointments like this:
+    //         {
+    //             "start": "2021-02-22T14:00:00+00:00",
+    //             "end": "2021-02-22T14:04:00+00:00",
+    //             "capacity": 1,
+    //             "remaining_spaces": -1
 
-            }
-        */
-        let remainingSpaces = currentValue["remaining_spaces"];
-        if (remainingSpaces > 0) {
-            const appointmentDateGMT = new Date(currentValue["start"]);
-            let appointmentDateET = appointmentDateGMT.toLocaleString("en-US", {
-                timeZone: "America/New_York",
-            });
-            appointmentDateET = appointmentDateET.substring(
-                0,
-                appointmentDateET.indexOf(",")
-            );
-            let dateAvailability = memo["availability"][appointmentDateET];
-            if (!dateAvailability) {
-                dateAvailability = {
-                    numberAvailableAppointments: 0,
-                    hasAvailability: false,
-                };
-                memo["availability"][appointmentDateET] = dateAvailability;
-            }
-            dateAvailability["numberAvailableAppointments"] += remainingSpaces;
-            dateAvailability["hasAvailability"] = true;
-            memo["hasAvailability"] = true;
-        }
-        return memo;
-    }, results);
-
+    //         }
+    //     */
+    //     let remainingSpaces = currentValue["remaining_spaces"];
+    //     if (remainingSpaces > 0) {
+    //         const appointmentDateGMT = new Date(currentValue["start"]);
+    //         let appointmentDateET = appointmentDateGMT.toLocaleString("en-US", {
+    //             timeZone: "America/New_York",
+    //         });
+    //         appointmentDateET = appointmentDateET.substring(
+    //             0,
+    //             appointmentDateET.indexOf(",")
+    //         );
+    //         let dateAvailability = memo["availability"][appointmentDateET];
+    //         if (!dateAvailability) {
+    //             dateAvailability = {
+    //                 numberAvailableAppointments: 0,
+    //                 hasAvailability: false,
+    //             };
+    //             memo["availability"][appointmentDateET] = dateAvailability;
+    //         }
+    //         dateAvailability["numberAvailableAppointments"] += remainingSpaces;
+    //         dateAvailability["hasAvailability"] = true;
+    //         memo["hasAvailability"] = true;
+    //     }
+    //     return memo;
+    // }, results);
     return results;
 }
 
