7247ced4d3b8fc3c920fd6be57b7c2b604e4e011
Paul Furbacher <pfurbacher@mac.com>
2021-03-25 12:44:10 -0400

    Initial commit
    - Rewritten to scrape from Acuity Scheduling pages.
    - Unit tests with mocked HTML

---
+++ site-scrapers/Harrington-acuity/config.js
@@ -0,0 +1,43 @@
+const site = {
+    name: "Harrington (local residents only)",
+    street: "153 Chestnut Street",
+    city: "Southbridge",
+    zip: "01550",
+    website:
+        "https://app.acuityscheduling.com/schedule.php?owner=22192301&calendarID=5202050",
+    restrictions:
+        "Registration is open to only those who live in the following towns: Auburn, Brimfield, Brookfield, (North, East, West) Charlton, Dudley, Holland, Leicester, Oxford, Southbridge, Spencer, Sturbridge, Sutton, Uxbridge, Wales, Warren, Webster",
+};
+
+const locations = [
+    {
+        name: "Harrington &emdash; local residents",
+        street: "153 Chestnut Street",
+        city: "Southbridge",
+        zip: "01550",
+        website:
+            "https://app.acuityscheduling.com/schedule.php?owner=22192301&calendarID=5202050",
+        restrictions:
+            "Registration is open to only those who live in the following towns: Auburn, Brimfield, Brookfield, (North, East, West) Charlton, Dudley, Holland, Leicester, Oxford, Southbridge, Spencer, Sturbridge, Sutton, Uxbridge, Wales, Warren, Webster",
+        signUpLink:
+            "https://app.acuityscheduling.com/schedule.php?owner=22192301&calendarID=5202050",
+    },
+];
+
+const site2 = {
+    name: "Harrington &emdash; non-local residents",
+    street: "153 Chestnut Street",
+    city: "Southbridge",
+    zip: "01550",
+    website:
+        "https://app.acuityscheduling.com/schedule.php?owner=22192301&calendarID=5202038",
+    signUpLink:
+        "https://app.acuityscheduling.com/schedule.php?owner=22192301&calendarID=5202038",
+};
+
+const monthCount = 3;
+
+module.exports = {
+    site,
+    monthCount,
+};
