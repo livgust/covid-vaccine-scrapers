df35e8b6f11c1df2ec191021569a916d49bfde19
tkjones117 <tkjones117@gmail.com>
2021-03-26 11:04:35 -0400

    maybe a code change is needed to trigger workflow?

--- scraper.js
+++ scraper.js
@@ -125,7 +125,7 @@ async function execute() {
         const webData = JSON.stringify(responseJson);
 
         if (process.env.DEVELOPMENT) {
-            //console.log("The following data would be published:");
+            console.log("The following data would be published:");
             //console.dir(responseJson, { depth: null });
             file.write("out.json", webData);
             return responseJson;
