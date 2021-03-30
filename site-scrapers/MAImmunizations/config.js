fd6c67945a3d3e47ad730bccfe34e643b2964ee1
Olivia Adams <liv.gust@gmail.com>
2021-03-30 13:54:22 -0400

    Revert "MAImmunization: Check for valid Sign up link (#196)"
    This reverts commit d30e46c26c4bfaddf020db3acd9e03aad9a6d7f1.

--- site-scrapers/MAImmunizations/config.js
+++ site-scrapers/MAImmunizations/config.js
@@ -3,7 +3,6 @@ const site = {
     website:
         "https://clinics.maimmunizations.org/clinic/search?q%5Bservices_name_in%5D%5B%5D=Vaccination&location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=&q%5Bclinic_date_gteq%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search&page=1#search_results",
     baseWebsite: "https://www.maimmunizations.org",
-    testSignUpLinkWebsite: "https://clinics.maimmunizations.org",
 };
 
 module.exports = {
