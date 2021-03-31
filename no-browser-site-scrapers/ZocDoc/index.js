8e15481813e26787b6f40dae323cfd0be3ee5a50
Paul Furbacher <pfurbacher@mac.com>
2021-03-31 14:46:27 -0400

    ZocDoc scraper (was: Tufts MC scraper) (#192)
    * Initial commit of scraper and unit tests
    
    * Initial ZocDoc commit
    
    - ZocDoc scraper for AFC Urgent Care and Holtzman.
    - Includes independent website for Tufts MC, also a ZocDoc powered site.
    - Config site content generator: gathers site info for the AFC UC and Holtzman locations. Makes it easier to gather the data should more sites be added to the main ZocDoc page.
    - Unit tests and supporting data files
    
    * Initial commit of sample data for unit tests
    
    * Code cleanup
    
    - Moved config generator code file writing from unit test file to the config generator file.
    
    * Updated to browserless scraper
    
    - Removed browser parameter in index.js and ZocDocTest.js
    - Unit test imports now point to no-browser-site-scrapers
    
    * Delete Tufts MC specific files; now a part of ZocDoc
    
    * Moved ZocDoc scraper and associated files to no-browser-site-scrapers
    
    - site-scrapers/ZocDoc -> no-browser-site-scrapers/ZocDoc
    - updated import in test file
    
    * Added back browser parameter to GetAvailableAppointments(), other changes
    
    - also changed to using moment in reformatDate()

---
+++ no-browser-site-scrapers/ZocDoc/index.js
@@ -0,0 +1,38 @@
+const { scraperName, sites } = require("./config");
+const helper = require("./zocdocBase");
+
+module.exports = async function GetAvailableAppointments(
+    _ignored,
+    fetchService = liveFetchService()
+) {
+    console.log(`${scraperName} starting.`);
+    const webData = await ScrapeWebsiteData(fetchService);
+    console.log(`${scraperName} done.`);
+    return webData;
+};
+
+function liveFetchService() {
+    return {
+        async fetchAvailability() {
+            return await helper.fetchAvailability();
+        },
+    };
+}
+
+async function ScrapeWebsiteData(fetchService) {
+    // Initialize results to no availability
+    const results = [];
+
+    const fetchedAvailability = await fetchService.fetchAvailability();
+    const allAvailability = helper.parseAvailability(fetchedAvailability);
+
+    Object.entries(allAvailability).forEach((value) => {
+        results.push({
+            ...sites[value[0]],
+            ...value[1],
+            hasAvailability: Object.keys(value[1].availability).length > 0,
+        });
+    });
+
+    return results;
+}
