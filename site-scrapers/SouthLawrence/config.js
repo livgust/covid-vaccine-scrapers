030d8da875a38a4fd8fedf00e83cb9d54c1508dd
dragthemagicpuffin <79419113+dragthemagicpuffin@users.noreply.github.com>
2021-04-03 21:53:40 -0400

    South Lawrence scraper (#114)
    * Pediatrics Associates of Greater Salem eslint OK TODO: will always show unavailable TODO: using the GetFilters graphql not the SearchAvailabilityDates
    
    * Replaced GetFilters with the SearchAvailabilityDates query.
    For 2 seconds there was availability and I had an example to work with. 
    But then I refreshed the browser and it was gone.
    Slots not implemented (no working examples).
    
    esLint OK
    
    * Resolve merge conflict with Walgreens
    
    * Distribute availability data to both locations.
    Slight refactor GetAvailableAppointments for easier "done" logging.
    
    * South Lawrence E. Elementary School
    1. GET the website and extract 2 IDs needed
    2. POST to get the calendar HTML fragment
    
    * Moved SouthLawrence to subfolder.
    Clean up re-submission of PAGS.
    Added a regex for the stand-by list.
    eslint OK
    
    * finish the move of S. Lawrence out of sites.json
    
    Co-authored-by: Olivia Adams <liv.gust@gmail.com>

---
+++ site-scrapers/SouthLawrence/config.js
@@ -0,0 +1,13 @@
+const site = {
+    name: "South Lawrence East Elementary School",
+    signUpLink: "https://lawrencegeneralcovidvaccine.as.me/schedule.php",
+    website: "https://lawrencegeneralcovidvaccine.as.me/schedule.php",
+    address: "165 Crawford Street",
+    city: "Lawrence",
+    state: "MA",
+    zip: "01843",
+};
+
+module.exports = {
+    site,
+};
