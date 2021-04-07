a1dcc163b9e7f08f29b380ca8ee62110d00d44ea
Daniel Jalkut <jalkut@red-sweater.com>
2021-04-07 14:53:19 -0400

    Initial support for scraping Albertsons (Shaw's / Star Market in MA). Unfortunately I can't put any more time in to tests for this at the moment. Hope it helps. #86

---
+++ no-browser-site-scrapers/Albertsons/config.js
@@ -0,0 +1,11 @@
+const site = {
+    name: "Albertsons",
+    website:
+        "https://www.mhealthappointments.com/covidappt",
+    nationalStoresJson:
+        "https://s3-us-west-2.amazonaws.com/mhc.cdn.content/vaccineAvailability.json?v=1617818069756",
+};
+
+module.exports = {
+    site,
+};
---
+++ no-browser-site-scrapers/Albertsons/index.js
@@ -0,0 +1,42 @@
+const { site } = require("./config");
+const { toTitleCase } = require("../../lib/stringUtil");
+const https = require("https");
+const { sendSlackMsg } = require("../../lib/slack");
+
+module.exports = async function GetAvailableAppointments() {
+    console.log(`${site.name} starting.`);
+    const webData = await ScrapeWebsiteData();
+    console.log(`${site.name} done.`);
+    
+    const massLocations = webData.filter((location) => location.region == "Massachusetts");
+    return massLocations.map((location) => {
+        const retval = {
+            name: `${site.name} (${location.address})`,
+            hasAvailability: location.availability,
+            signUpLink: location.coach_url,
+            latitude: location.lat,
+            longitude: location.long,
+        }
+        return retval
+    })
+};
+
+function urlContent(url, options) {
+    return new Promise((resolve) => {
+        let response = "";
+        https.get(url, options, (res) => {
+            let body = "";
+            res.on("data", (chunk) => (body += chunk));
+            res.on("end", () => {
+                response = body;
+                resolve(response);
+            });
+        });
+    });
+}
+
+async function ScrapeWebsiteData() {
+    const url = `${site.nationalStoresJson}`;
+    const options = { headers: { Referer: site.website } };
+    return JSON.parse(await urlContent(url, options));
+}
