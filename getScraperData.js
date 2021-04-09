2177d117a4ce25e6b64131755cea62ea36d2a6d5
Olivia Adams <liv.gust@gmail.com>
2021-04-09 14:39:16 -0400

    read from DB + cache result for 5s

--- getScraperData.js
+++ getScraperData.js
@@ -1,18 +1,10 @@
-const AWS = require("aws-sdk");
-
-const s3 = new AWS.S3();
-
+const scraperData = require("./lib/db/scraper_data");
 exports.handler = async () => {
-    const data = await s3
-        .getObject({
-            Bucket: "ma-covid-vaccine",
-            Key: "data.json",
-        })
-        .promise();
+    const data = await scraperData.getAppointmentsForAllLocations();
 
     const response = {
         statusCode: 200,
-        body: JSON.parse(data.Body.toString()),
+        body: JSON.stringify(data),
     };
     return response;
 };
