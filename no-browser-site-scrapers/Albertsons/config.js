a283b6806305af972e5924e833ee8805f3dadba0
Daniel Jalkut <jalkut@red-sweater.com>
2021-04-10 07:49:45 -0400

    Support for scraping Albertsons (Shaw's / Star Market in MA) (#221)
    * Initial support for scraping Albertsons (Shaw's / Star Market in MA). Unfortunately I can't put any more time in to tests for this at the moment. Hope it helps. #86
    
    * Try to fix the expected boolean type of hasAvailability.
    
    * Run Prettier on source files.
    
    * Compute the timestamp parameter for the Albertsons JSON URL rather than hardcoding it.
    
    * Pull apart the long address components to supply more detailed name, city, street, state, and zip.
    
    * Run prettier on previous commit.
    
    * Convert HTTP request to use node-fetch, clean up some cruft, and add timestamp to resulting location data.
    
    * Fix street address to use the right label.

---
+++ no-browser-site-scrapers/Albertsons/config.js
@@ -0,0 +1,10 @@
+const site = {
+    name: "Albertsons",
+    website: "https://www.mhealthappointments.com/covidappt",
+    nationalStoresJson:
+        "https://s3-us-west-2.amazonaws.com/mhc.cdn.content/vaccineAvailability.json",
+};
+
+module.exports = {
+    site,
+};
---
+++ no-browser-site-scrapers/Albertsons/index.js
@@ -0,0 +1,40 @@
+const { site } = require("./config");
+const https = require("https");
+const fetch = require("node-fetch");
+
+module.exports = async function GetAvailableAppointments() {
+    console.log(`${site.name} starting.`);
+
+    const url = `${site.nationalStoresJson}?v=${new Date().valueOf()}`;
+
+    const nationalLocations = await fetch(url).then((res) => res.json());
+
+    const massLocations = nationalLocations.filter(
+        (location) => location.region == "Massachusetts"
+    );
+
+    console.log(`${site.name} done.`);
+
+    return massLocations.map((location) => {
+        // Raw address is like: (Star Market 4587 - 45 William T Morrissey Blvd, Dorchester, MA, 02125)
+        // The format seems to be very consistent nationally, not to mention locally in MA. So we
+        // pull the specific parts out of the string.
+        const rawAddress = location.address;
+        const trimmedAddress = rawAddress.replace(/^\(|\)$/, ""); // Trim parens
+        const [name, longAddress] = trimmedAddress.split(" - ");
+        const [street, city, state, zip] = longAddress.split(", ");
+        const retval = {
+            name: name,
+            city: city,
+            street: street,
+            state: state,
+            zip: zip,
+            hasAvailability: location.availability == "true",
+            signUpLink: location.coach_url,
+            latitude: location.lat,
+            longitude: location.long,
+            timestamp: new Date(),
+        };
+        return retval;
+    });
+};
