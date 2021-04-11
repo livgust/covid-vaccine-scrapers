cea52498d8036c367db7a6e1c87f4bdd09b2b8b3
Paul Furbacher <pfurbacher@mac.com>
2021-04-11 12:21:16 -0400

    Rewrite of the Harrington scraper (#180)
    * Initial commit
    
    - Rewritten to scrape from Acuity Scheduling pages.
    - Unit tests with mocked HTML
    
    * Cosmetic issue fixed: Changed text in one "it()"
    
    * Moved to Harrington folder
    
    * Updated folder reference int require() due to move of scraper to Harrington folder
    
    * Updated to scrape restricted and unrestricted sites.
    
    * Initial commit
    
    - Rewritten to scrape from Acuity Scheduling pages.
    - Unit tests with mocked HTML
    
    * Cosmetic issue fixed: Changed text in one "it()"
    
    * Moved to Harrington folder
    
    * Updated folder reference int require() due to move of scraper to Harrington folder
    
    * Updated to scrape restricted and unrestricted sites.
    
    * Changed allResults to { {result1}, {result2} }; was [ {result1}, {result2} ]
    
    Had thought that other multi-site scrapers were returning arrays of site results. After reading through the multi-site scrapers (MA, Hannaford, etc.) realized this needed to be changed to { {}, {} }.
    
    * Changes requested in PR review
    
    - town-restricted -> Local Residents
    - unrestricted -> All Eligible Residents
    - add timestamp property to results
    - catch block log.out -> log.err
    - cleaned up unused vars in the test file
    
    * Changed returned results to [] from {}
    
    * Changes due to results [] being returned.
    
    - See previous commit.
    
    * Corrected availability structure from [] to {}

--- site-scrapers/Harrington/index.js
+++ site-scrapers/Harrington/index.js
@@ -1,100 +1,255 @@
-const { site } = require("./config");
-const https = require("https");
+const s3 = require("../../lib/s3");
+const { sendSlackMsg } = require("../../lib/slack");
 const moment = require("moment");
 
-module.exports = async function GetAvailableAppointments(browser) {
-    return null;
-    // THIS SITE NO LONGER USES SIGNUP.COM. SEE GITHUB ISSUE 165
-    // console.log(`${site.name} starting.`);
-    // const {
-    //     name,
-    //     website,
-    //     signUpDotComLink,
-    //     signUpLink,
-    //     restrictedWebsite,
-    //     restrictedSignUpDotComLink,
-    //     restrictedSignUpLink,
-    //     ...restHarrington
-    // } = site;
-    // const webData = await ScrapeWebsiteData(website, signUpDotComLink, browser);
-    // const restrictedWebData = await ScrapeWebsiteData(
-    //     restrictedWebsite,
-    //     restrictedSignUpDotComLink,
-    //     browser
-    // );
-    // console.log(`${site.name} done.`);
+const {
+    entity,
+    townRestricted,
+    unRestricted,
+    monthCount,
+} = require("./config.js");
 
-    // return [
-    //     {
-    //         ...restHarrington,
-    //         ...webData,
-    //         name: `${name} (All MA residents)`,
-    //         signUpLink: signUpLink,
-    //         timestamp: moment().format(),
-    //     },
-    //     {
-    //         ...restHarrington,
-    //         ...restrictedWebData,
-    //         name: `${name} (Local residents)`,
-    //         signUpLink: restrictedSignUpLink,
-    //         timestamp: moment().format(),
-    //         restrictions:
-    //             "Residents of the following towns, no exceptions: Auburn, Leicester, Southbridge, Sturbridge, Charlton, Spencer, Brookfield (North, East, West), Brimfield, Wales, Holland, Warren, Dudley, Webster, Oxford, and Sutton",
-    //     },
-    // ];
-};
-/*
-async function ScrapeWebsiteData(website, signUpDotComLink, browser) {
-    const p = new Promise((resolve) => {
-        let response = "";
-        //todayDate looks like "2020%2F12%2F16"
-        let todayDate = moment().local().format();
-        todayDate = todayDate
-            .substring(0, todayDate.indexOf("T"))
-            .replace(/-/g, "%2F");
-        https.get(`${website}&startdate=${todayDate}`, (res) => {
-            let body = "";
-            res.on("data", (chunk) => {
-                body += chunk;
-            });
-            res.on("end", () => {
-                response = JSON.parse(body);
-                resolve(response.data);
-            });
+module.exports = async function GetAvailableAppointments(
+    browser,
+    pageService = defaultPageService(),
+    customNotificationService
+) {
+    console.log(`${entity} starting.`);
+    if (customNotificationService) {
+        notificationService = customNotificationService;
+    }
+
+    const allResults = [];
+
+    try {
+        const townRestrictedAvailability = await ScrapeWebsiteData(
+            browser,
+            pageService,
+            townRestricted
+        );
+
+        allResults.push({
+            ...townRestricted,
+            ...townRestrictedAvailability,
+            timestamp: moment().format(),
         });
-    });
-    const data = await p;
-    const results = { availability: {}, hasAvailability: false };
-    for (const dateEntry of data) {
-        //force midnight local time zone to avoid UTC dateline issues
-        const date = moment(dateEntry.edate).local();
-        const slotsAvailable =
-            dateEntry.jobstotal - dateEntry.jobstotalassignments;
-        results.availability[
-            `${date.month() + 1}/${date.date()}/${date.year()}`
-        ] = {
-            numberAvailableAppointments: slotsAvailable,
-            hasAvailability: !!slotsAvailable,
-        };
-        if (slotsAvailable) {
-            results.hasAvailability = true;
+
+        const unRestrictedAvailability = await ScrapeWebsiteData(
+            browser,
+            pageService,
+            unRestricted
+        );
+
+        allResults.push({
+            ...unRestricted,
+            ...unRestrictedAvailability,
+            timestamp: moment().format(),
+        });
+    } catch (error) {
+        console.error(`Harrington :: GetAvailableAppointments(): ${error}`);
+    }
+
+    console.log(`${entity} done.`);
+
+    return allResults;
+};
+
+/**
+ availabilityContentSavedToS3
+
+ Unused at the moment. If notificationService is activated, use this flag to
+ limit saving to s3 to only once.
+
+ Suggested usage:
+
+ When `dates` are found in `getDailyAvailabilityCountsForMonth()`
+ ```
+ ...
+    const dates = await page.$$eval(".time-selection", (slots) =>
+        slots.map((slot) => slot.getAttribute("data-readable-date"))
+    );
+    // notify s3 and Slack
+    if (!availabilityContentSavedToS3) {
+        await notificationService.handleAvailabilityContentUpdate(page);
+        availabilityContentSavedToS3 = true;
+    }
+    ...
+```
+ */
+let availabilityContentSavedToS3 = false; // becomes true upon first save to s3
+/* eslint-disable no-unused-vars */
+/* eslint-enable no-unused-vars */
+
+/**
+ * The notificationService provides functionality to
+ * notify s3 and Slak of changes to availability.
+ *
+ * Replace with mocks in unit testing.
+ *
+ * Not used at the moment, but retained in case there's a future
+ * need to capture HTML due to changes in the website.
+ */
+let notificationService = function defaultNotificationService() {
+    return {
+        async handleAvailabilityContentUpdate(page, site) {
+            return notifyAvailabilityContentUpdate(page, site);
+        },
+    };
+};
+
+async function ScrapeWebsiteData(browser, pageService, site) {
+    console.log(`${site.name} starting to scrape`);
+    const page = await pageService.getHomePage(browser, site);
+
+    // Initialize results to no availability
+    const results = {
+        availability: {},
+        hasAvailability: false,
+    };
+
+    /*
+     monthCount is set in config.js; limit it to only a few months because this style
+     of Acuity Scheduling calendar is endless going forward in time. There is
+     no month chooser drop-down to grab a list of months.
+     */
+    for (const key of new Array(monthCount).keys()) {
+        // Don't advance the calendar if it's the first month
+        if (key > 0) {
+            await pageService.getNextMonthCalendar(page, site);
         }
+        const dailySlotsForMonth = await getDailyAvailabilityCountsForMonth(
+            page
+        );
+        // Add all day objects to results.availability
+        dailySlotsForMonth.forEach((value, key) => {
+            results.availability[key] = {
+                numberAvailableAppointments: value,
+                hasAvailability: !!value,
+            };
+        });
     }
-    // check if the event is locked via the UI
-    if (results.hasAvailability && signUpDotComLink) {
-        const page = await browser.newPage();
-        await page.goto(signUpDotComLink);
-        await page
-            .waitForSelector("#error_page", {
-                timeout: 20000, //20s
-            })
-            .then(() => {
-                //if we hit the error page, the event is locked.
-                results.availability = {};
-                results.hasAvailability = false;
-            })
-            .catch(() => {}); //if this times out, then we're good!
-    }
+
+    results.hasAvailability = !!Object.keys(results.availability).length;
+
+    console.log(`${site.name} scrape ending`);
+
     return results;
 }
-*/
+
+/**
+ * Each slot is respresented by the following HTML:
+ *
+ *    &lt;input type="radio"
+ *        class="time-selection"
+ *        name="time[]"
+ *        data-qa="select-day-29-slot-0"
+ *        data-readable-date="2021-03-29"
+ *        value="2021-03-29 11:45"
+ *        id="appt1617032700"
+ *        aria-labelledby="lbl_appt1617032700"&gt;
+ *
+ * Select for ".time-selection" and evaluate the "data-readable-date" attribute
+ * to get the date. Appointment time (in "value" attribute) isn't needed.
+ *
+ * Map date from 'yyyy-mm-dd' to 'm/d/yyyy'.
+ *
+ * Group by date, accumulating date count, into a Map: key = date, value = slot count for date.
+ *
+ * @param {*} page
+ * @returns Map of availability keyed by date. Guaranteed not null, returning at least an empty map.
+ */
+async function getDailyAvailabilityCountsForMonth(page) {
+    function reformatDate(date) {
+        const dateObj = new Date(date + "T00:00:00");
+        return new Intl.DateTimeFormat("en-US").format(dateObj);
+    }
+
+    /*
+        The website seems to have changed to "alert-danger" for its "no
+        times available" message. Keeping the old way ("#no-times-availabile-message")
+        in case they switch back.
+     */
+    const noTimesAvailable = await page.$("#no-times-available-message");
+    const alertDanger = await page.$("#alert-danger");
+    if (noTimesAvailable || alertDanger) {
+        return new Map();
+    }
+    let dailySlotCountsMap; // keyed by date, value accumulates slot counts per date.
+
+    try {
+        const dates = await page.$$eval(".time-selection", (slots) =>
+            slots.map((slot) => slot.getAttribute("data-readable-date"))
+        );
+
+        dailySlotCountsMap = dates
+            .map((date) => reformatDate(date))
+            .reduce((acc, date) => {
+                acc.set(date, (acc.get(date) || 0) + 1);
+                return acc;
+            }, new Map());
+    } catch (error) {
+        console.error(`error trying to get day numbers: ${error}`);
+    }
+
+    return dailySlotCountsMap ? dailySlotCountsMap : new Map();
+}
+
+/**
+ * Advances the page to the next month by clicking on the "next" button.
+ *
+ * The monthCount variable in config.js limits how many times this is executed.
+ *
+ * @param {*} page
+ */
+async function advanceMonth(page, site) {
+    try {
+        await Promise.all([
+            /*
+                The ability to click on the next month button seems to
+                be quite flaky. This one seems to work for the moment,
+                without having to waitForSelector().
+            */
+            await page.evaluate(() =>
+                document.querySelector(".calendar-next").click()
+            ),
+        ]);
+    } catch (error) {
+        console.error(`${site.name} :: trouble advancing button: ${error}`);
+    }
+}
+
+/**
+ * The pageService provides functions which retrieve pages.
+ *
+ * Replace with mocks in unit testing.
+ */
+function defaultPageService() {
+    return {
+        async getHomePage(browser, site) {
+            const classToWaitFor = "#step-pick-appointment";
+            const page = await browser.newPage();
+            await Promise.all([
+                page.goto(site.website),
+                waitForLoadComplete(page, classToWaitFor),
+            ]);
+            return page;
+        },
+
+        async getNextMonthCalendar(page, site) {
+            return advanceMonth(page, site);
+        },
+    };
+}
+
+async function notifyAvailabilityContentUpdate(page, responseText, site) {
+    const msg = `${site.name} - possible appointments:\n${responseText}`;
+    await sendSlackMsg("bot", msg);
+    await s3.savePageContent(site.name, page);
+}
+
+async function waitForLoadComplete(page, loaderSelector) {
+    await page.waitForSelector(loaderSelector, {
+        visible: true,
+        timeout: 15000,
+    });
+}
