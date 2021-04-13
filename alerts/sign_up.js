9fdb82eacb5bf2a23b375ff2642d0863c80f9a37
Olivia Adams <liv.gust@gmail.com>
2021-04-12 22:14:01 -0400

    add functions to manage subscriptions

---
+++ alerts/sign_up.js
@@ -0,0 +1,149 @@
+const faunadb = require("faunadb"),
+    fq = faunadb.query;
+const dbUtils = require("../lib/db/utils");
+
+const signUp = {
+    activateSubscription,
+    addOrUpdateSubscription,
+    cancelSubscription,
+    getSubscription,
+};
+
+module.exports = signUp;
+
+async function addOrUpdateSubscription({ phoneNumber, email, zip, radius }) {
+    const data = {
+        phoneNumber,
+        email,
+        zip,
+        radius,
+        active: false,
+        cancelled: false,
+    };
+    if (phoneNumber && email) {
+        throw new Error("phone number and email both set!");
+    }
+    if (phoneNumber) {
+        return dbUtils.faunaQuery(
+            fq.If(
+                fq.Exists(fq.Match("subscriptionByPhoneNumber", phoneNumber)),
+                fq.Update(
+                    fq.Select(
+                        "ref",
+                        fq.Get(
+                            fq.Match("subscriptionByPhoneNumber", phoneNumber)
+                        )
+                    ),
+                    {
+                        data,
+                    }
+                ),
+                fq.Create(fq.Collection("subscriptions"), { data })
+            )
+        );
+    } else if (email) {
+        return dbUtils.faunaQuery(
+            fq.If(
+                fq.Exists(fq.Match("subscriptionByEmail", email)),
+                fq.Update(
+                    fq.Select(
+                        "ref",
+                        fq.Get(fq.Match("subscriptionByEmail", email))
+                    ),
+                    {
+                        data,
+                    }
+                ),
+                fq.Create(fq.Collection("subscriptions"), { data })
+            )
+        );
+    } else {
+        throw new Error("Neither email nor phone number were set!");
+    }
+}
+
+async function cancelSubscription({ phoneNumber, email }) {
+    if (phoneNumber && email) {
+        throw new Error("both phone number and email defined!");
+    } else if (phoneNumber) {
+        return dbUtils.faunaQuery(
+            fq.Update(
+                fq.Select(
+                    "ref",
+                    fq.Get(fq.Match("subscriptionByPhoneNumber", phoneNumber))
+                ),
+                { data: { cancelled: true } }
+            )
+        );
+    } else if (email) {
+        return dbUtils.faunaQuery(
+            fq.Update(
+                fq.Select(
+                    "ref",
+                    fq.Get(fq.Match("subscriptionByEmail", email))
+                ),
+                { data: { cancelled: true } }
+            )
+        );
+    } else {
+        throw new Error("no phone or email set!");
+    }
+}
+
+async function activateSubscription({ phoneNumber, email }) {
+    if (phoneNumber && email) {
+        throw new Error("both phone number and email defined!");
+    } else if (phoneNumber) {
+        return dbUtils.faunaQuery(
+            fq.Update(
+                fq.Select(
+                    "ref",
+                    fq.Get(fq.Match("subscriptionByPhoneNumber", phoneNumber))
+                ),
+                { data: { active: true } }
+            )
+        );
+    } else if (email) {
+        return dbUtils.faunaQuery(
+            fq.Update(
+                fq.Select(
+                    "ref",
+                    fq.Get(fq.Match("subscriptionByEmail", email))
+                ),
+                { data: { active: true } }
+            )
+        );
+    } else {
+        throw new Error("no phone or email set!");
+    }
+}
+
+async function getSubscription({ phoneNumber, email }) {
+    if (phoneNumber && email) {
+        throw new Error("both phone number and email defined!");
+    } else if (phoneNumber) {
+        return dbUtils.faunaQuery(
+            fq.If(
+                fq.Exists(fq.Match("subscriptionByPhoneNumber", phoneNumber)),
+                fq.Select(
+                    "data",
+                    fq.Get(fq.Match("subscriptionByPhoneNumber", phoneNumber))
+                ),
+                null
+            )
+        );
+    } else if (email) {
+        return dbUtils.faunaQuery(
+            fq.If(
+                fq.Exists(fq.Match("subscriptionByEmail", email)),
+                fq.Select(
+                    "data",
+                    fq.Get(fq.Match("subscriptionByEmail", email))
+                ),
+                null
+            )
+        );
+    } else {
+        throw new Error("no phone or email set!");
+    }
+}
---
+++ test/alerts/sign_up.js
@@ -0,0 +1,95 @@
+const sinon = require("sinon");
+const signUp = require("../../alerts/sign_up");
+const chai = require("chai");
+const expect = chai.expect;
+const moment = require("moment");
+const { faunaQuery } = require("../../lib/db/utils");
+
+describe("getSubscription", () => {
+    it("errors if you send both phone and email", async () => {
+        let e;
+        try {
+            await signUp.getSubscription({
+                phoneNumber: "8578675309",
+                email: "email@test.com",
+            });
+        } catch (error) {
+            e = error;
+        }
+        expect(e).to.be.instanceOf(Error);
+    });
+});
+describe("addSubscription", () => {
+    it("sets and retrieves a phone number subscription", async () => {
+        await signUp.addOrUpdateSubscription({
+            phoneNumber: "8578675309",
+            zip: "12345",
+            radius: 100,
+        });
+        expect(
+            await signUp.getSubscription({ phoneNumber: "8578675309" })
+        ).to.deep.equal({
+            phoneNumber: "8578675309",
+            zip: "12345",
+            radius: 100,
+            active: false,
+            cancelled: false,
+        });
+    });
+    it("sets and retrieves an email subscription", async () => {
+        await signUp.addOrUpdateSubscription({
+            email: "test@example.com",
+            zip: "12345",
+            radius: 100,
+        });
+        expect(
+            await signUp.getSubscription({ email: "test@example.com" })
+        ).to.deep.equal({
+            email: "test@example.com",
+            zip: "12345",
+            radius: 100,
+            active: false,
+            cancelled: false,
+        });
+    });
+});
+
+describe("activateSubscripition", () => {
+    it("sets active to true", async () => {
+        await signUp.addOrUpdateSubscription({
+            email: "test@example.com",
+            zip: "12345",
+            radius: 100,
+        });
+        await signUp.activateSubscription({ email: "test@example.com" });
+        expect(
+            await signUp.getSubscription({ email: "test@example.com" })
+        ).to.deep.equal({
+            email: "test@example.com",
+            zip: "12345",
+            radius: 100,
+            active: true,
+            cancelled: false,
+        });
+    });
+});
+
+describe("cancelSubscripition", () => {
+    it("sets cancelled to true", async () => {
+        await signUp.addOrUpdateSubscription({
+            email: "test@example.com",
+            zip: "12345",
+            radius: 100,
+        });
+        await signUp.cancelSubscription({ email: "test@example.com" });
+        expect(
+            await signUp.getSubscription({ email: "test@example.com" })
+        ).to.deep.equal({
+            email: "test@example.com",
+            zip: "12345",
+            radius: 100,
+            active: false,
+            cancelled: true,
+        });
+    });
+});
