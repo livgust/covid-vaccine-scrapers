5f729f10bf8288cf9acb878d98d3dcd9ae2e84da
Paul Furbacher <pfurbacher@mac.com>
2021-04-15 23:43:46 -0400

    Whittier health scraper (#238)
    * Initial commit
    
    * Renamed test html file
    
    * Added a sampe HTML file containing one slot
    
    * Finalized approach to scraping slots from page
    
    * Adding tests
    
    * Renamed fetchCalendarLinks to fetchFrontPage
    
    * Format cleanup and extensive javadoc additions
    
    * Refactored 'signUpIsFull()' to eliminate try/catch
    
    * More comment cleanup
    
    * Added one no availability file and changed name
    
    * Added to comments
    
    * Deletion caused by renaming file
    
    * Replaced ||= with standard lhs || rhs

---
+++ no-browser-site-scrapers/WhittierHealth/config.js
@@ -0,0 +1,11 @@
+const site = {
+    name: "Whittier Rehabilitation Hospital",
+    street: "145 Ward Hill Ave",
+    city: "Haverhill",
+    zip: "01835",
+    signUpLink: "https://www.whittierhealth.com/covid-vaccine-clinics/",
+};
+
+module.exports = {
+    site,
+};
