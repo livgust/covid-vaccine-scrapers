b81e68e8dfdf0faf50c4f51499ba2b9f2a41b63b
Olivia Adams <liv.gust@gmail.com>
2021-04-15 22:30:52 -0400

    throttle texts

--- alerts/send_texts_and_emails.js
+++ alerts/send_texts_and_emails.js
@@ -3,6 +3,8 @@ const determineRecipients = require("./determine_recipients");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const SMS_PER_SECOND = 20;
+
 if (process.env.NODE_ENV !== "production") {
     AWS.config.credentials = new AWS.SharedIniFileCredentials({
         profile: "macovidvaccines",
@@ -29,32 +31,43 @@ async function handler({ locations, numberAppointmentsFound, message }) {
 }
 
 async function sendTexts(phoneNumbers, message) {
-    return new Promise((resolve, reject) =>
-        pinpoint.sendMessages(
-            {
-                ApplicationId: process.env.PINPOINT_APPLICATION_ID,
-                MessageRequest: {
-                    Addresses: phoneNumbers.reduce((obj, curNum) => {
-                        obj[`+1${curNum}`] = { ChannelType: "SMS" };
-                        return obj;
-                    }, {}),
-                    MessageConfiguration: {
-                        SMSMessage: {
-                            Body: message,
-                            MessageType: "PROMOTIONAL",
-                            OriginationNumber:
-                                process.env.PINPOINT_ORIGINATION_NUMBER,
+    const phoneNumbersCopy = [...phoneNumbers];
+    while (phoneNumbersCopy.length) {
+        const currentPhoneNumbers = phoneNumbersCopy.splice(
+            0,
+            Math.min(SMS_PER_SECOND, phoneNumbersCopy.length)
+        );
+        console.log(`sending texts to ${phoneNumbersCopy}`);
+        await new Promise((resolve, reject) =>
+            pinpoint.sendMessages(
+                {
+                    ApplicationId: process.env.PINPOINT_APPLICATION_ID,
+                    MessageRequest: {
+                        Addresses: phoneNumbers.reduce((obj, curNum) => {
+                            obj[`+1${curNum}`] = { ChannelType: "SMS" };
+                            return obj;
+                        }, {}),
+                        MessageConfiguration: {
+                            SMSMessage: {
+                                Body: message,
+                                MessageType: "PROMOTIONAL",
+                                OriginationNumber:
+                                    process.env.PINPOINT_ORIGINATION_NUMBER,
+                            },
                         },
                     },
                 },
-            },
-            (err, data) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(data);
+                (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
                 }
-            }
-        )
-    );
+            )
+        );
+        if (phoneNumbersCopy.length) {
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+    }
 }
