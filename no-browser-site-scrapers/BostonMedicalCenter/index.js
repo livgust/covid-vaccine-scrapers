0698a6a5cc05721d0914566f33c0ced00dfd03ce
Paul Furbacher <pfurbacher@mac.com>
2021-04-19 14:11:18 -0400

    Dynamic site id lookup added

--- no-browser-site-scrapers/BostonMedicalCenter/index.js
+++ no-browser-site-scrapers/BostonMedicalCenter/index.js
@@ -1,24 +1,19 @@
-const { sites, providerIDs } = require("./config");
+const { entityName, settingsUrl, signUpLink } = require("./config");
 const mychart = require("../../lib/MyChartAPI");
 const moment = require("moment");
-
-const departmentIDs = sites.map((site) => site.departmentID);
-const vt = "2008";
+const { default: fetch } = require("node-fetch");
 
 module.exports = async function GetAvailableAppointments() {
-    console.log("BMC starting.");
+    console.log(`${entityName} staring.`);
     const webData = await ScrapeWebsiteData();
-    console.log("BMC done.");
-    const results = [];
+    console.log(`${entityName} done.`);
+
+    const results = Object.values(webData);
+    for (const result of results) {
+        result["signUpLink"] = signUpLink;
+    }
     const timestamp = moment().format();
 
-    for (const site of sites) {
-        const { departmentID, ...restSite } = site;
-        results.push({
-            ...webData[site.departmentID],
-            ...restSite,
-        });
-    }
     return {
         parentLocationName: "Boston Medical Center",
         isChain: true,
@@ -28,14 +23,16 @@ module.exports = async function GetAvailableAppointments() {
 };
 
 async function ScrapeWebsiteData() {
-    // We need to go through the flow and use a request verification token
+    const data = await getSiteData();
+
+    // Request verification token to use in further access
     const [
         cookie,
         verificationToken,
     ] = await mychart.GetCookieAndVerificationToken(
-        `https://mychartscheduling.bmc.org/mychartscheduling/SignupAndSchedule/EmbeddedSchedule?id=${providerIDs.join(
+        `https://mychartscheduling.bmc.org/mychartscheduling/SignupAndSchedule/EmbeddedSchedule?id=${data.providerIds.join(
             ","
-        )}&dept=${departmentIDs.join(",")}&vt=${vt}&lang=en-US`
+        )}&dept=${data.departmentIds.join(",")}&vt=${data.vt}&lang=en-US`
     );
 
     return mychart.AddFutureWeeks(
@@ -44,6 +41,42 @@ async function ScrapeWebsiteData() {
         cookie,
         verificationToken,
         10,
-        mychart.CommonPostDataCallback(providerIDs, departmentIDs, vt)
+        mychart.CommonPostDataCallback(
+            data.providerIds,
+            data.departmentIds,
+            data.vt
+        )
     );
+}
+
+/**
+ * Get all possible provider and respective department IDs, and the vt (visit type) for BMC.
+ *
+ * Kudos to the folks at VaccineTimes
+ * (https://github.com/ginkgobioworks/vaccinetime/blob/main/lib/sites/my_chart.rb)
+ * who somehow discovered this settings URL.
+ *
+ * @returns
+ */
+async function getSiteData() {
+    const text = await fetch(`${settingsUrl()}`)
+        .then((res) => res.text())
+        .then((text) => {
+            return text;
+        })
+        .catch((error) =>
+            console.error(`failed to get BMC providers from setting: ${error}`)
+        );
+
+    /*
+        Looking for content in the following:
+
+        url: "https://mychartscheduling.bmc.org/mychartscheduling/SignupAndSchedule/EmbeddedSchedule?id=10033909,10033319,10033364,10033367,10033370,10033706,10033083,10033373&dept=10098252,10098245,10098242,10098243,10098244,10105138,10108801,10098241&vt=2008",
+    */
+    const relevantText = text.match(/EmbeddedSchedule.+/)[0];
+    const ids = relevantText.match(/id=([\d,]+)&/)[1].split(",");
+    const deptIds = relevantText.match(/dept=([\d,]+)&/)[1].split(",");
+    const vt = relevantText.match(/vt=(\d+)/)[1];
+
+    return { providerIds: ids, departmentIds: deptIds, vt: vt };
 }
