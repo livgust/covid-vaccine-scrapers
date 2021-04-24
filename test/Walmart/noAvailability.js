35ba776b8a06e256e08aeda6f540c691241e99c9
Paul Furbacher <pfurbacher@mac.com>
2021-04-23 22:15:40 -0400

    Initial commit (#248)

---
+++ test/Walmart/noAvailability.js
@@ -0,0 +1,13 @@
+const noAvailability = {
+    status: "1",
+    message: "OK",
+    data: {
+        startDate: "04212021",
+        endDate: "04272021",
+        accessPointId: "607052ab-8410-47ec-ba1c-388a1597cfc2",
+    },
+};
+
+module.exports = {
+    noData: noAvailability,
+};
