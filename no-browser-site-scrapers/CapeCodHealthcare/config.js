d1f3039d97ee41725d2edbde11a0847dca4db89c
Rob Harris <harcod@users.noreply.github.com>
2021-04-25 21:25:54 -0400

    Cape Cod Hospital scraper (#251)

---
+++ no-browser-site-scrapers/CapeCodHealthcare/config.js
@@ -0,0 +1,12 @@
+const site = {
+    name: "Hyannis Melody Tent",
+    street: "21 West Main St",
+    city: "Hyannis",
+    zip: "02601",
+    signUpLink:
+        "https://mychart-openscheduling.et1149.epichosted.com/MyChart/OpenScheduling",
+};
+
+module.exports = {
+    site,
+};
