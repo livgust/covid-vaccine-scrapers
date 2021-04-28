d238696affb793dc9ebd3ce30b1152e5e5fc541c
Anthony Salmeron <anthonysalmerondev@gmail.com>
2021-04-28 19:28:25 -0400

    Revere popup clinics

---
+++ site-scrapers/ReverePopup/config.js
@@ -0,0 +1,13 @@
+const site = {
+    name: "Revere Board of Health Popup Clinic",
+    street: "140 American Legion Hwy",
+    city: "Revere",
+    zip: "02151",
+    signUpLink: "https://www.maimmunizations.org//reg/4012583569",
+    restrictions: "For those who live and work in Revere",
+    extraData: "Moderna",
+};
+
+module.exports = {
+    site,
+};
