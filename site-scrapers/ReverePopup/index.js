b3af7dd6e8cf21430b9ab8a6bdd3f05518a8d60c
Anthony Salmeron <anthonysalmerondev@gmail.com>
2021-05-04 18:29:39 -0400

    Removing Revere-specific scrapers for now

--- site-scrapers/ReverePopup/index.js
+++ site-scrapers/ReverePopup/index.js
@@ -19,47 +19,48 @@ module.exports = async function GetAvailableAppointments(browser) {
 };
 
 async function ScrapeWebsiteData(browser) {
-    const page = await browser.newPage();
-    const results = {
-        hasAvailability: false,
-        availability: {},
-    };
-    await page.goto(site.signUpLink);
+    return { hasAvailability: false };
+    // const page = await browser.newPage();
+    // const results = {
+    //     hasAvailability: false,
+    //     availability: {},
+    // };
+    // await page.goto(site.signUpLink);
     // Only 1 h1 on page, will have text like "Sign Up for Vaccinations - Rumney Marsh Academy on 04/30/2021"
-    try {
-        await page.waitForSelector("h1", { visible: true });
-        const date = await page.$eval(
-            "h1",
-            (h1) => h1.innerText.match(/[0-9]+\/[0-9]+\/[0-9]+/)[0]
-        );
+    // try {
+    //     await page.waitForSelector("h1", { visible: true });
+    //     const date = await page.$eval(
+    //         "h1",
+    //         (h1) => h1.innerText.match(/[0-9]+\/[0-9]+\/[0-9]+/)[0]
+    //     );
 
-        // the first tr on the page is unrelated which is why you have to be specific
-        const appointments = await page.$$eval("tr[data-parent]", (trs) =>
-            trs.reduce((acc, tr) => {
-                // text will look like "06:09 pm\n\n20 appointments available"
-                const text = tr.innerText;
-                const numberAvailableAppointments = text.match(
-                    /([0-9]+) appointments available/
-                );
-                if (numberAvailableAppointments) {
-                    return Number(numberAvailableAppointments[1]) + acc;
-                }
-                return acc;
-            }, 0)
-        );
+         // the first tr on the page is unrelated which is why you have to be specific
+    //     const appointments = await page.$$eval("tr[data-parent]", (trs) =>
+    //         trs.reduce((acc, tr) => {
+    //             // text will look like "06:09 pm\n\n20 appointments available"
+    //             const text = tr.innerText;
+    //             const numberAvailableAppointments = text.match(
+    //                 /([0-9]+) appointments available/
+    //             );
+    //             if (numberAvailableAppointments) {
+    //                 return Number(numberAvailableAppointments[1]) + acc;
+    //             }
+    //             return acc;
+    //         }, 0)
+    //     );
 
-        if (appointments) {
-            results["hasAvailability"] = true;
-            results["availability"][date] = {
-                numberAvailableAppointments: appointments,
-                hasAvailability: true,
-            };
-            return results;
-        } else {
-            return results;
-        }
-    } catch (error) {
-        console.log("Error in Revere PopUp clinic:", error);
-        return results;
-    }
+    //     if (appointments) {
+    //         results["hasAvailability"] = true;
+    //         results["availability"][date] = {
+    //             numberAvailableAppointments: appointments,
+    //             hasAvailability: true,
+    //         };
+    //         return results;
+    //     } else {
+    //         return results;
+    //     }
+    // } catch (error) {
+    //     console.log("Error in Revere PopUp clinic:", error);
+    //     return results;
+    // }
 }
