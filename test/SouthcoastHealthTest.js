053c343f36bad4d45bfa49611815842f234b756b
Paul Furbacher <pfurbacher@mac.com>
2021-05-08 02:48:44 -0400

    Added expect assertions

--- test/SouthcoastHealthTest.js
+++ test/SouthcoastHealthTest.js
@@ -12,6 +12,19 @@ describe("SouthCoastHealth :: test Fall River sample JSON", () => {
         //
         const fallRiverResults = parseJson(fallRiverJson);
 
+        expect(Object.keys(fallRiverResults).length).equals(3);
+
+        const totalSlots = Object.values(fallRiverResults).reduce(
+            (acc, value) => {
+                acc += value.numberAvailableAppointments;
+                return acc;
+            },
+            0
+        );
+
+        const timeStrMatch = JSON.stringify(fallRiverJson).match(/time/g);
+        expect(totalSlots).equals(timeStrMatch.length);
+
         if (process.env.DEVELOPMENT) {
             file.write(
                 `${process.cwd()}/out_FallRiver-SouthCoastHealth.json`,
