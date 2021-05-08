84936de1ce438922c7018b26231c039ce0a6b5ee
Paul Furbacher <pfurbacher@mac.com>
2021-05-08 13:55:54 -0400

    Revert "Merge branch 'walmart' of https://github.com/livgust/covid-vaccine-scrapers"
    This reverts commit a7ea3b69d966fbd70e8070bf236a484c96619376, reversing
    changes made to 1b86d44d237f2cd5a5db3d293f10b9db829f0a5f.

--- site-scrapers/Walmart/config.js
+++
@@ -1,9 +0,0 @@
-const entityName = "Walmart";
-
-const loginUrl =
-    "https://www.walmart.com/account/login?returnUrl=%2Fpharmacy%2Fclinical-services%2Fimmunization%2Fscheduled%3FimzType%3Dcovid";
-
-module.exports = {
-    entityName,
-    loginUrl,
-};
