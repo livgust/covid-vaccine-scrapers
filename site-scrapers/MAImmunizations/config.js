b75df2b40df638e5569cf4e2397ccbd027250ad8
Rob Harris <harcod@users.noreply.github.com>
2021-05-17 21:57:54 -0400

    site changed the search URL (#278)

--- site-scrapers/MAImmunizations/config.js
+++ site-scrapers/MAImmunizations/config.js
@@ -1,7 +1,7 @@
 const site = {
     name: "MAImmunizations",
     website:
-        "https://clinics.maimmunizations.org/clinic/search?q%5Bservices_name_in%5D%5B%5D=Vaccination&location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=&q%5Bclinic_date_gteq%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search&page=1#search_results",
+        "https://clinics.maimmunizations.org/appointment/en/clinic/search?location=&search_radius=All&q%5Bvenue_search_name_or_venue_name_i_cont%5D=&clinic_date_eq%5Byear%5D=&clinic_date_eq%5Bmonth%5D=&clinic_date_eq%5Bday%5D=&q%5Bvaccinations_name_i_cont%5D=&commit=Search&page=1#search_results",
     baseWebsite: "https://www.maimmunizations.org",
     testSignUpLinkWebsite: "https://clinics.maimmunizations.org",
 };
